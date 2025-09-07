32eef534c632130a3c663238defc348d18cf1b41
Ganesh Tellamekala <ganeshtellamekala5@gmail.com>
2025-09-07 10:50:23 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -8,6 +8,7 @@ public class Student {
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
         System.out.println("=======================");
+        system.out.println("JOB IS COMPLETED THANKS")
          System.out.println("WELCOME TO JENKINS");
         System.out.println("JENKINS WEBHOOK JOB");
         System.out.println("WEBHOOK ");
